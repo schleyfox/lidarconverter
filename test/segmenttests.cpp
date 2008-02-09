35e82d8453bca96a65d8e2e75bd8dffade56d7a5
ben <ben@b8611051-9045-0410-8e59-9b3d6e8491ad>
2008-02-09 22:58:41 +0000

    Added test for Travel SW across Prime Meridian
    * Oslo to Douglas
    
    
    git-svn-id: http://svn.pixelmachine.org/lidarconverter/trunk@32 b8611051-9045-0410-8e59-9b3d6e8491ad

--- test/segmenttests.cpp
+++ test/segmenttests.cpp
@@ -22,6 +22,18 @@ void SegmentTests::heading() {
 	Helper::makeKMLPath(cape_town_to_beijing, "kml/cape_town_to_beijing.kml");
 	QCOMPARE(fuzzy_round(cape_town_to_beijing.heading().degs(), 2),
 			fuzzy_round(Angle::Degrees(-58.31).degs(),2));
+
+	//Oslo to Douglas
+	DataPoint* oslo = new DataPoint;
+	oslo->setLat(59.9333); oslo->setLon(10.75);
+	DataPoint* douglas = new DataPoint;
+	douglas->setLat(54.1452); douglas->setLon(-4.4817);
+	Segment oslo_to_douglas;
+	oslo_to_douglas += Helper::makeStraightLineDataPoints(oslo,
+		       	douglas, 10000, data);
+	Helper::makeKMLPath(oslo_to_douglas, "kml/oslo_to_douglas.kml");
+	QCOMPARE(fuzzy_round(oslo_to_douglas.heading().degs(), 2),
+			fuzzy_round(Angle::Degrees(123.18).degs(),2));
 }
 
 void SegmentTests::fullAngle(){ }

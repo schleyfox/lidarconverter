f29a3a4424faa1b82fcc5acb85f9cc18c14cad49
ben <ben@b8611051-9045-0410-8e59-9b3d6e8491ad>
2008-03-03 18:33:39 +0000

    Wrapped the DynamicDataSource interface in DataSourceWidget
    git-svn-id: http://svn.pixelmachine.org/lidarconverter/trunk@97 b8611051-9045-0410-8e59-9b3d6e8491ad

--- gui/datasourcewidget.h
+++ gui/datasourcewidget.h
@@ -8,13 +8,64 @@ class DataSourceWidget : public QWidget {
 	
 	DynamicDataSource* toDataSource();
 	QString toXml();
+	
+	//DynamicDataSource wrapping, getters
+	QMap<int, int> resolutions() {
+		return resmap->toMap();
+	}
+	QString longitudeDataName() {
+		return lon_line->text();
+	}
+	QString latitudeDataName() {
+		return lat_line->text();
+	}
+	QString dataName() {
+		return data_line->text();
+	}
+	int maxAltitude() {
+		max_alt_spin->value();
+	}
+	int baseHResolution() {
+		return base_horiz_spin->value();
+	}
+	int bottomOffset() {
+		return offset_spin->value();
+	}
+	bool inverted() {
+		return (invert_box->checkState() == Qt::Checked) ? true : false;
+	}
+
 
 	public slots:
 	void import(QString filename);
 	void import();
 	void save(QString filename);
 	void save();
-
+	//DynamicDataSource wrapping, setters
+	void setResolutions(QMap<int, int> resolutions) {
+		resmap->fromMap(resolutions);
+	}
+	void setLongitudeDataName(QString longitudedataname){
+		lon_line->setText(longitudedataname);
+	}
+	void setLatitudeDataName(QString latitudedataname){
+		lat_line->setText(latitudedataname);
+	}
+	void setDataName(QString dataname){
+		data_line->setText(dataname);
+	}
+	void setMaxAltitude(int maxaltitude) {
+		max_alt_spin->setValue(maxaltitude);
+	}
+	void setBaseHResolution(int h_res) {
+		base_horiz_spin->setValue(h_res);
+	}
+	void setBottomOffset(int offset) {
+		offset_spin->setValue(offset);
+	}
+	void setInverted(bool invert) {
+		invert_box->setCheckState((invert) ? Qt::Checked : Qt::Unchecked);
+	}
 
 	protected:
 	DynamicDataSource* ds;
